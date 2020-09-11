5193c4f926f53e6f9006226d4e329953e79b4cfd
João Soares <joaobat111@gmail.com>
2020-09-11 10:10:29 -0300

    commit

--- components/footer/index.js
+++ components/footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='bg-gray-700 p-4'>
       <div className='container mx-auto text-center font-bold text-white'>
-        Projeto Desenvolvido por: {' '} João Soares / {' '}
+        Projeto Desenvolvido por: {'João Soares'}
         <a className='px-2 hover:underline' href='https://www.linkedin.com/in/jo%C3%A3o-soares-45363bb7/'>Linkedin</a> / {' '}
         <a className='px-2 hover:underline' href='https://github.com/JoaoBatiistadeSousaSoares'>GitHub</a>
         <div className='mt-2'>
